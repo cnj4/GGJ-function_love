1bc154f9b3746d758c63527e911dab45b09d97e2
C. Crosby <cnj4@njit.edu>
2016-01-30 01:15:40 -0500

    unity project + first signs of a parser + language spec.

---
+++ Assets/Interpreter.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Interpreter : MonoBehaviour {
+    //TODO: fix delimiting
+    private char[] delimiters = [' ','[',']','{','}','(',')',';','+','-','=','/',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '];
+    private string[] keywords = ["function", "for", "while", "int", "bool", "string", "flaot"];
+    public string incode = ""
+/*
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+*/
+    string Parse (inCode){
+
+        char[] inCodeArray = inCode.ToCharArray();
+
+        for(int i = 0; i < inCodeArray.Length(); i++){
+            char currChar = inCodeArray[i];
+
+            //TODO: Parse Chars
+
+        }
+
+
+    }
+
+}
